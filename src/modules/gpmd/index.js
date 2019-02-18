0d9e11296921b7d1cc0cb7941d948e7268283897
vexii <vexii@5am.dk>
2019-02-18 19:23:10 +0100

    gmusic socket start

--- src/modules/gpmd/index.js
+++ src/modules/gpmd/index.js
@@ -5,7 +5,33 @@ import {
   dispatch,
   registerReducer
 } from 'store'
+import Websocket from 'ws'
 
-const initialState = {
-  playing: Boolean,
+const PLAYSTATE_CHANGE: 'PLAYSTATE_CHANGE' = 'PLAYSTATE_CHANGE'
+
+function playStateChange(isPlaying: boolean) {
+  return ({
+    type: PLAYSTATE_CHANGE,
+    payload: isPlaying,
+  })
 }
+
+let connected = false;
+
+const ws = new Websocket('ws://localhost:5672')
+ws.on('open', (...args) => {
+  connected = true;
+})
+
+ws.on('message', (data) => {
+  const { channel, payload } = JSON.parse(data)
+  switch (channel) {
+    case 'playState': {
+      return dispatch(playStateChange(payload))
+    } 
+  }
+  if(channel === 'playState') {
+    console.log(channel, payload)
+  }
+})
+
